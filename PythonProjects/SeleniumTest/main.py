20fbcab2fec5b3262384e53beb1c2438f51ac24a
GdeGuz25 <Gdeguz@mail.ru>
2025-07-03 15:17:07 +0500

    Add files via upload

--- PythonProjects/SeleniumTest/main.py
+++ PythonProjects/SeleniumTest/main.py
@@ -2,29 +2,49 @@ import requests
 
 URL = 'https://api.pokemonbattle.ru/v2'
 Token = '4f10a21aafec52bad2dbcb07be66f9b1'
-Header = {'Content-Type' : 'application/json','trainer_token':Token }
+Header = {'Content-Type' : 'application/json','trainer_token': Token}
+
+body_confirmation = {
+    "trainer_token": Token
+}
 
 body_create = {
     "name": "Ulybka",
-    "photo_id": -1
-}
-body_change = {
-    "pokemon_id": "348991",
-    "name": "Milashka",
-    "photo_id": 2
-}
-body_catch = {
-    "pokemon_id": "348991"
-}
+    "photo_id": 619
+    }
 
 response_create = requests.post(url=f'{URL}/pokemons', headers=Header, json= body_create)
 print(response_create.text)
+if response_create.status_code == 200:
+   print(f"Error creating Pokemon: {response_create.text}")
+else:
+   pokemon_id = response_create.json().get('id')
+   if not pokemon_id:
+      raise ValueError("Pokemon ID is missing in the response")
+   else:
+      print(f"Pokemon created with ID:{pokemon_id}")
+
+
+body_rename = {
+    "id": "pokemon_id",
+    "name": "Milashka",
+    }
 
 message = response_create.json()['message']
 print(message)
 
-response_change = requests.put(url=f'{URL}/pokemons', headers=Header, json=body_change)
-print(response_change.text)
+response_rename = requests.put(url=f'{URL}/pokemons',headers=Header, json=body_rename)
+if response_rename.status_code == 200:
+  print(f"Error renaming Pokemon:{response_rename.text}")
+else:
+   print("Pokemon renamed successfully.")
 
-response_catch = requests.post(url=f'{URL}/trainers/add_pokeball', headers=Header, json=body_catch)
-print(response_catch.text)
+body_catch = {
+    "pokemon_id": pokemon_id
+}
+
+response_catch = requests.post(url=f'{URL}/pokemons/add_pokeball', headers=Header, json=body_catch)
+if response_catch.status_code == 200:
+   print(f"Error catching Pokemon: {response_catch.text}")
+else:
+   print("Pokemon caught successfully.")
